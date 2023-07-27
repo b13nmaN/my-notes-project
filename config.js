c8af911a39366b46d2a8278a52ace1fc7bc63e68
Kyle King <KingKyleTech@outlook.com>
2023-07-26 21:59:55 -0300

    Update config.js
    Removed your API key @b13nmaN

--- config.js
+++ config.js
@@ -7,14 +7,8 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCt9XC-CdhsJ6GMPwGehL99I7ggsigGf3I",
-  authDomain: "note-app-1f3a4.firebaseapp.com",
-  projectId: "note-app-1f3a4",
-  storageBucket: "note-app-1f3a4.appspot.com",
-  messagingSenderId: "521291440467",
-  appId: "1:521291440467:web:529d8fc62dd2d7e6c401a6"
-};
+const firebaseConfig; 
+// Get your own firebase access token 
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
